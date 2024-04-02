b081d5082d3e10273577a4cb0f78ff89f37279bb
Алекей <148230062+vervest4334133@users.noreply.github.com>
2024-04-02 19:51:40 +0300

    Delete word directory

--- word/cnt/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class Word(models.Model):
-    file = models.FileField()
-
--- word/cnt/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from word.cnt.views import word_list
-
-urlpatterns = [
-    path('index/', word_list, name='index'),
-]
